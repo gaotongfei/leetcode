2ee5ff4eed5a7c1dbc452901b345a1ebe82a7a44
Taff Gao <gaotongfei1995@gmail.com>
2017-06-22 17:52:10 +0800

    Add project files.

---
+++ LeetCodeSolution/TwoSum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolution
+{
+    public class Solution
+    {
+        public int[] ReturnIndices = new int[2];
+        public int Sum;
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = i + 1; j < nums.Length; j++)
+                {
+                    Sum = nums[i] + nums[j];
+                    if (Sum == target)
+                    {
+                        ReturnIndices[0] = i;
+                        ReturnIndices[1] = j;
+                    }
+                }
+            }
+            return ReturnIndices;
+        }
+    }
+}
---
+++ LeetCodeSolutionTests1/TwoSumTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeSolution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolution.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void TwoSumTest()
+        {
+            var solution = new Solution();
+            var result = solution.TwoSum(new int[] {2, 7, 11, 15}, 9);
+            CollectionAssert.AreEqual(result, new int[] {0, 1});
+
+            var result2 = solution.TwoSum(new int[] {2, 8, 11, 15}, 10);
+            CollectionAssert.AreEqual(result, new int[] {0, 1});
+
+            var result3 = solution.TwoSum(new int[] {2, 8, 11, 15}, 19);
+            CollectionAssert.AreEqual(result, new int[] {1, 2});
+        }
+    }
+}
